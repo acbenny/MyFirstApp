4bb5fdfbd2a50119e2c0a63a87eca7ab06729874
Arun Benny <acbenny@gmail.com>
2014-08-26 20:29:17 +0530

    First commit. Working app

---
+++ src/com/acbenny/myfirstapp/DisplayMessageActivity.java
@@ -0,0 +1,46 @@
+package com.acbenny.myfirstapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_display_message);
+		Intent intent = getIntent();
+		
+		String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+		
+		if (!(message == null || message.equalsIgnoreCase(""))){
+			TextView textview = (TextView)findViewById(R.id.displayMessageTextView);
+			Log.e("acb","message:"+message+".");
+			textview.setText(message); 
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.display_message, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
