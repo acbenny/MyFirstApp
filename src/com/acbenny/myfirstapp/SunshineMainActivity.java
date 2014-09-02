edabb963eecc26d6cb122d4eeff0f78030abcae0
Arun Benny <acbenny@gmail.com>
2014-09-02 22:25:50 +0530

    Added refresh button that fetches actual weather data from internet

--- src/com/acbenny/myfirstapp/SunshineMainActivity.java
+++ src/com/acbenny/myfirstapp/SunshineMainActivity.java
@@ -2,8 +2,6 @@ package com.acbenny.myfirstapp;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class SunshineMainActivity extends Activity {
 
@@ -15,24 +13,5 @@ public class SunshineMainActivity extends Activity {
 			getFragmentManager().beginTransaction()
 					.add(R.id.container, new ForecastFragment()).commit();
 		}
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.sunshine_main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
 	}
 }
